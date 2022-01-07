f774faf155cf2d5730fac80834301406b7eb3c40
Stan Nowak <samestan.nowak@gmail.com>
2022-01-07 12:25:16 -0800

    Change cv pdf file link

--- src/routes/cv.svelte
+++ src/routes/cv.svelte
@@ -1,5 +1,5 @@
 <svelte:head>
     <title>Stan Nowak:CV</title>
 </svelte:head>
-<iframe title= "cv" src="cv0918.pdf#toolbar=0&view=FitH" width="100%" height="700px">
+<iframe title= "cv" src="CV2022.pdf#toolbar=0&view=FitH" width="100%" height="700px">
 </iframe>
