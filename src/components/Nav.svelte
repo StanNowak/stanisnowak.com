4848a9687642b8242473d72ba0cb6fcc565d239a
Stan Nowak <samestan.nowak@gmail.com>
2022-08-19 11:47:05 -0700

    Update Nav Font

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -8,11 +8,15 @@
 		font-weight: 700;
 		padding: 0 1em;
 		font-family: "Source Sans Pro";
+		font-weight: 700;
+		
 	}
 
 	ul {
 		margin: 0;
 		padding: 0;
+		font-family: "Source Sans Pro";
+		font-weight: 700;
 	}
 
 	/* clearfix */
@@ -54,9 +58,10 @@
 </style>
 
 <nav>
-	<ul>
+	<ul style="font-family: Lato">
 		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">Home</a></li>
 		<li><a rel=prefetch aria-current="{segment === 'cv' ? 'page' : undefined}" href="cv">CV</a></li>
+		
 		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 		     the blog data when we hover over the link or tap it on a touchscreen -->
 		<!-- <li><a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">Blog</a></li> -->
