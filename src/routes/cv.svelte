99377f6b18c926e33a4453e6dc14351c2ccc9ff2
Stan Nowak <samestan.nowak@gmail.com>
2022-11-15 22:34:29 -0800

    Update cv.svelte

--- src/routes/cv.svelte
+++ src/routes/cv.svelte
@@ -1,5 +1,5 @@
 <svelte:head>
     <title>Stan Nowak CV</title>
 </svelte:head>
-<iframe title= "cv" src={`${StanNowakCV.pdf}#view=fitH`}  width="100%" height="100%">
+<iframe title= "cv" src="StanNowakCV.pdf#toolbar=0&view=FitH"  width="100%" height="800px">
 </iframe>
