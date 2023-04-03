8bc72d7dbd2e48a08afe3f301d476c8fb21f34c7
Stan Nowak <samestan.nowak@gmail.com>
2023-04-03 11:50:03 -0700

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,18 @@
   import '../app.css';
 </script>
 
+<svelte:head>
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-9MEDMN3QKR"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-9MEDMN3QKR');
+  </script>
+</svelte:head>
+
 <style>
   :root {
     --bg-color: #1a1a1a;
