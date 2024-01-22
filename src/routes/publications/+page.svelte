36adb4f24c22ff27968fff389f71dfb487fdb21f
Stan Nowak <samestan.nowak@gmail.com>
2024-01-21 23:28:51 -0800

    pubs update

--- src/routes/publications/+page.svelte
+++ src/routes/publications/+page.svelte
@@ -1,84 +1,117 @@
 <script>
 	// Sample JSON data
 	const publications = [
-		{
-			id: 1,
-			title: 'I’m Not Sure: Designing for Ambiguity in Visual Analytics',
-			authors: ['Stan Nowak', 'Lyn Bartram'],
-			venue: 'Graphics Interface 2022',
-			date: '2022',
-			type: 'Archival Conference Proceedings (refereed)',
-			link: 'https://openreview.net/pdf?id=SHQU_yejZFv',
-			thumbnail: '/Thumbnails/GI22.png',
-			video: null
-		},
-		{
-			id: 7,
-			title: 'Give Me the Data: Visual Analytics Needs to Go Beyond Visualization',
-			thumbnail: '/Thumbnails/ASCR.png',
-			authors: ['Stan Nowak', 'Lyn Bartram'],
-			venue:
-				'ASCR Workshop on Visualization for Scientific Discovery, Decision-Making, & Communication 2022',
-			link: 'https://custom.cvent.com/DCBD4ADAAD004096B1E4AD96F3C8049E/files/event/fc8f3c090e35418982b417f2d3e4e73e/5e4a011ffb0044bd8afb37ff00124c74.pdf',
-			video: null
-		},
-		{
-			id: 5,
-			title: 'Visual Analytics: A Method to Explore Natural Histories of Oral Epithelial Dysplasia',
-			authors: ['Stan Nowak', 'Miriam Rosing', 'Wolfgang Stuerzlinger', 'Lyn Bartram'],
-			venue: 'Frontiers in Oral Health 2021',
-			date: '2021',
-			type: 'Journal',
-			link: 'https://www.frontiersin.org/articles/10.3389/froh.2021.703874/full',
-			thumbnail: '/Thumbnails/Frontier21.jpeg',
-			video: null
-		},
-		{
-			id: 2,
-			title: 'Designing for Ambiguity: Visual Analytics in Avalanche Forecasting',
-			authors: ['Stan Nowak', 'Lyn Bartram', 'Pascal Haegeli'],
-			venue: 'IEEE Visualization Conference 2020',
-			date: '2020',
-			type: 'Archival Conference Proceedings (refereed)',
-			link: 'https://arxiv.org/abs/2009.02800',
-			thumbnail: '/Thumbnails/VizShort20.png',
-			video: null
-		},
-		{
-			id: 6,
-			title:
-				'Enhancing the operational value of snowpack models with visualization design principles',
-			authors: ['Simon Horton', 'Stan Nowak', 'Pascal Haegeli'],
-			venue: 'Natural Hazards and Earth System Sciences 2019',
-			date: '2019',
-			type: 'Journal',
-			link: 'https://nhess.copernicus.org/articles/20/1557/2020/',
-			thumbnail: '/Thumbnails/NHESS20.png',
-			video: null
-		},
-		{
-			id: 3,
-			title: 'A Micro-Phenomenological Lens for Evaluating Narrative Visualization',
-			authors: ['Stan Nowak', 'Lyn Bartram', 'Thecla Schiphorst'],
-			venue: 'IEEE Evaluation and Beyond-Methodological Approaches for Visualization 2018',
-			date: '2018',
-			type: 'Archival Conference Proceedings (refereed)',
-			link: 'https://osf.io/ws7j9/',
-			thumbnail: '/Thumbnails/BELIV18.png',
-			video: null
-		},
-		{
-			id: 4,
-			title: 'Exploring regional snowpack patterns with gridded models',
-			authors: ['Simon Horton', 'Stan Nowak', 'Pascal Haegeli'],
-			venue: 'International Snow Science Workshop Proceedings 2018',
-			date: '2018',
-			type: 'Archival Conference Proceedings (refereed)',
-			link: 'http://www.avalancheresearch.ca/wp-content/uploads/2018/08/2018_ISSW_Horton_SnowpackPatterns.pdf',
-			thumbnail: '/Thumbnails/ISSW18.png',
-			video: null
-		}
-	];
+    {
+        id: 10,
+        title: 'Designing for ambiguity in sensemaking: Visual analytics in risk analysis and prediction',
+        authors: ['Stan Nowak'],
+        venue: 'Ph.D. thesis, Simon Fraser University',
+        date: '2023',
+        type: 'Thesis',
+        link: 'https://summit.sfu.ca/_flysystem/fedora/2023-11/etd22736.pdf',
+        thumbnail: '',
+        video: null
+    },
+    {
+        id: 9,
+        title: 'Identifying Visualization Opportunities to Help Architects Manage the Complexity of Building Codes',
+        authors: ['Stan Nowak', 'Bon Adriel Aseniero', 'Lyn Bartram', 'Tovi Grossman', 'George Fitzmaurice', 'Justin Matejka'],
+        venue: 'IEEE Computer Graphics & Applications',
+        date: '2023',
+        type: 'Journal',
+        link: 'https://www.research.autodesk.com/app/uploads/2023/10/Identifying_Visualization_Opportunities_to_Help_Architects_Manage_the_Complexity_of_Building_Codes.pdf',
+        thumbnail: '',
+        video: null
+    },
+    {
+        id: 8,
+        title: 'Designing for Ambiguity in Visual Analytics: Lessons from Risk Assessment and Prediction',
+        authors: ['Stan Nowak', 'Lyn Bartram'],
+        venue: 'IEEE Transactions on Visualization and Computer Graphics',
+        date: '2023',
+        type: 'Journal',
+        link: 'https://osf.io/y35u4/',
+        thumbnail: '',
+        video: null
+    },
+    {
+        id: 7,
+        title: 'Give Me the Data: Visual Analytics Needs to Go Beyond Visualization',
+        authors: ['Stan Nowak', 'Lyn Bartram'],
+        venue: 'ASCR Workshop on Visualization for Scientific Discovery, Decision-Making, & Communication 2022',
+        date: '2022',
+        type: 'Archival Conference Proceedings (refereed)',
+        link: 'https://custom.cvent.com/DCBD4ADAAD004096B1E4AD96F3C8049E/files/event/fc8f3c090e35418982b417f2d3e4e73e/5e4a011ffb0044bd8afb37ff00124c74.pdf',
+        thumbnail: '/Thumbnails/ASCR.png',
+        video: null
+    },
+    {
+        id: 6,
+        title: 'Visual Analytics: A Method to Explore Natural Histories of Oral Epithelial Dysplasia',
+        authors: ['Stan Nowak', 'Miriam Rosing', 'Wolfgang Stuerzlinger', 'Lyn Bartram'],
+        venue: 'Frontiers in Oral Health 2021',
+        date: '2021',
+        type: 'Journal',
+        link: 'https://www.frontiersin.org/articles/10.3389/froh.2021.703874/full',
+        thumbnail: '',
+        video: null
+    },
+    {
+        id: 5,
+        title: 'Designing for Ambiguity: Visual Analytics in Avalanche Forecasting',
+        authors: ['Stan Nowak', 'Lyn Bartram', 'Pascal Haegeli'],
+        venue: 'IEEE Visualization Conference 2020',
+        date: '2020',
+        type: 'Archival Conference Proceedings (refereed)',
+        link: 'https://arxiv.org/abs/2009.02800',
+        thumbnail: '',
+        video: null
+    },
+    {
+        id: 4,
+        title: 'Enhancing the operational value of snowpack models with visualization design principles',
+        authors: ['Simon Horton', 'Stan Nowak', 'Pascal Haegeli'],
+        venue: 'Natural Hazards and Earth System Sciences 2019',
+        date: '2019',
+        type: 'Journal',
+        link: 'https://nhess.copernicus.org/articles/20/1557/2020/',
+        thumbnail: '',
+        video: null
+    },
+    {
+        id: 3,
+        title: 'A Micro-Phenomenological Lens for Evaluating Narrative Visualization',
+        authors: ['Stan Nowak', 'Lyn Bartram', 'Thecla Schiphorst'],
+        venue: 'IEEE Evaluation and Beyond-Methodological Approaches for Visualization 2018',
+        date: '2018',
+        type: 'Archival Conference Proceedings (refereed)',
+        link: 'https://osf.io/ws7j9/',
+        thumbnail: '',
+        video: null
+    },
+    {
+        id: 2,
+        title: 'Exploring regional snowpack patterns with gridded models',
+        authors: ['Simon Horton', 'Stan Nowak', 'Pascal Haegeli'],
+        venue: 'International Snow Science Workshop Proceedings 2018',
+        date: '2018',
+        type: 'Archival Conference Proceedings (refereed)',
+        link: 'http://www.avalancheresearch.ca/wp-content/uploads/2018/08/2018_ISSW_Horton_SnowpackPatterns.pdf',
+        thumbnail: '',
+        video: null
+    },
+    {
+        id: 1,
+        title: 'I’m Not Sure: Designing for Ambiguity in Visual Analytics',
+        authors: ['Stan Nowak', 'Lyn Bartram'],
+        venue: 'Graphics Interface 2022',
+        date: '2022',
+        type: 'Archival Conference Proceedings (refereed)',
+        link: 'https://openreview.net/pdf?id=SHQU_yejZFv',
+        thumbnail: '',
+        video: null
+    }
+];
 </script>
 <div class="page-container">
 <h1>Publications</h1>
